fd3fae46ecb7f29420d4df1e65d79898ffb27cbb
anas faiz <142715278+anassaahi@users.noreply.github.com>
2023-10-17 23:23:24 +0500

    Add files via upload

---
+++ week1/chair.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"     ______________ "<<endl;
+cout<<"    |             | "<<endl;
+cout<<"    |             |"<<endl;
+cout<<"    |             |"<<endl;
+cout<<"    |             |"<<endl;
+cout<<"    |_____________| "<<endl;
+cout<<"    |\            \ "<<endl;
+cout<<"    | \            \ "<<endl;
+cout<<"    |  \            \ "<<endl;
+cout<<"    |   \            \ "<<endl;
+cout<<"    |    \____________\ "<<endl;
+cout<<"    |    |            | "<<endl;
+cout<<"         |            | "<<endl;
+cout<<"         |            | "<<endl;
+return 0;
+
+ }
